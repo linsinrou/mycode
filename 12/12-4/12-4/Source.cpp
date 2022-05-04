ae3e2e748a2f86745aa5e66a5913b5b0479692c3
linsinrou <101238809+linsinrou@users.noreply.github.com>
2022-05-04 15:35:21 +0800

    Add files via upload

--- 12/12-4/12-4/Source.cpp
+++ 12/12-4/12-4/Source.cpp
@@ -1,42 +1,75 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
-double stuAve(vector<vector<double>>& x)
+void stuAve(vector<vector<double>>& x)
 {
     for (int i = 0; i < 5; i++)
     {
         double total = 0;
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < 3; j++)
         {
             total += x[i][j];
-            x[i][4] = total / 3;
         }
-        return x[i][4];
-    } 
+        x[i][3] = total / 3.0;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << fixed << setprecision(1) << x[i][j] << ",";
+        }
+        cout << fixed << setprecision(1) << x[i][3] << endl;
+    }
+    
 }
+
+double* courseAve(vector<vector<double>> x)
+{
+    double* a = new double[3];
+    for (int i = 0; i < 3; i++)
+    {
+        double total = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            total += x[j][i];
+        }
+        a[i] = total / 5.0;
+    }
+    return a;
+}
+
+
+
 int main()
 {
-    vector< vector<double>> x;
-    double a[5][4] = {
+    vector<vector<double>> x(5,vector<double>(4));
+    double a[5][3] = {
         {80.0, 70.0, 90.0},
         {85.0, 77.0, 95.0},
         {83.0, 75.0, 70.0},
         {73.0, 95.0, 67.0},
         {88.0, 79.0, 71.0},
     };
+    double* b;
+    
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < 3; j++)
         {
             x[i][j] = a[i][j];
         }
     }
-    cout << stuAve(x);
+    stuAve(x);
+    b = courseAve(x);
+    cout << "Average:";
+    for (int i = 0; i < 2; i++)
+    {
+        cout << fixed << setprecision(1) << b[i] << ",";
+    }
+    cout << fixed << setprecision(1) << b[2];
     
-
-
-
 
     return 0;
 }
